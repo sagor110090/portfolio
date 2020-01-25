ff9634b25a2e457dd3358ce9233cd422115f8295
sagor110090 <hasan15-4933@diu.edu.bd>
2020-01-25 16:54:39 +0600

    navbar active

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
     <header>
-      <nav class="navbar navbar-expand-lg">
-        <div class="container">
-          <a href="index.html" class="custom-logo-link" rel="home">
-            <img src="@/assets/images/logo.png" class="custom-logo" alt="Amike" />
-          </a>
-            
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul id="menu-primary" class="navbar-nav ml-auto">
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">
+                <a href="index.html" class="custom-logo-link" rel="home">
+                    <img src="./assets/images/logo.png" class="custom-logo" alt="Amike" />
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul id="menu-primary" class="navbar-nav ml-auto">
               <li class="nav-link" @click="start">
-                <router-link class="router-link-exact-active router-link-active" to="/">Home</router-link>
+                <router-link to="/">Home</router-link>
               </li>
               <li class="nav-link" @click="start">
                 <router-link to="/about">About</router-link>
@@ -25,9 +27,9 @@
                 <router-link to="/contact">Contact</router-link>
               </li>
             </ul>
-          </div>
-        </div>
-      </nav>
+                </div>
+            </div>
+        </nav>
     </header>
     <vue-progress-bar></vue-progress-bar>
 
@@ -60,4 +62,8 @@ li {
 a {
   color: red;
 }
+.router-link-exact-active{
+  color: white;
+}
+
 </style>
